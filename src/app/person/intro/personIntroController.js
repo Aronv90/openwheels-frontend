46fa617702afa5efee409c105bd0b08c8bb9722b
Kelley van Evert <kelley@mywheels.nl>
2019-10-09 11:28:36 +0200

    dashboard api call bugfixing + version bump

--- src/app/person/intro/personIntroController.js
+++ src/app/person/intro/personIntroController.js
@@ -2,7 +2,7 @@
 
 angular.module('owm.person.intro', [])
 
-.controller('PersonIntroController', function ($scope, me, $state, metaInfoService, appConfig) {
+.controller('PersonIntroController', function ($scope, me, $state, metaInfoService, Analytics, appConfig) {
 
   metaInfoService.set({url: appConfig.serverUrl + '/dashboard/intro'});
   metaInfoService.set({canonical: 'https://mywheels.nl/dashboard/intro'});
@@ -15,7 +15,8 @@ angular.module('owm.person.intro', [])
   }
 
   if(me.status !== 'new') {
-    $state.go('owm.person.dashboard');
+    Analytics.trackEvent('buglogging', 'redirect_intro_to_dashboard', me.id + '_' + me.status, undefined, true);
+    // $state.go('owm.person.dashboard');
   }
 
   $scope.toggleBox = function (box) {
